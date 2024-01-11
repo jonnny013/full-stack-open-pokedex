09ea066fd044fb008ec496d311c85646b8c2230b
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-11 14:03:56 +0800

    fix configure file Add before each to test add new test for clicking a page Finish exercises up to 11.9

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      baseUrl: 'http://localhost:5000'
-    },
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:5000',
   },
-});
+})
