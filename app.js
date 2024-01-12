f96fb82a4cf29b95122d69f0002075e980650396
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-12 16:43:08 +0800

    adjust fly

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-
+// eslint-disable-next-line no-console
+  console.log('Health check request received')
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
